9add0b27f6f44b4319a4de4daf9ade51820e01ba
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-05-19 11:07:21 +0530

    19 th session continution is added

--- Hello World/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Hello World/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //R.Java
+    //1.enable the developer options in the device
+    //Seettings >> about Phone>> BuildNumber(7 times continues)
+    // >> one message you have a develeoper options
+
+    //In the developer options we have to enable three things
+    // 1. OEM Unlock, 2.USB debugging.3.Verify Apps Over uSB(Install apps Via USB)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
